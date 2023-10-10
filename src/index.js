c99a25ef3ae94e5930e118c9cacab9b23d7793d6
B1ckbeard <46893211+B1ckbeard@users.noreply.github.com>
2023-10-10 12:51:52 +0300

    step 3

--- src/index.js
+++ src/index.js
@@ -1,46 +1,71 @@
 import './styles.scss';
 import 'bootstrap';
 import * as yup from 'yup';
+import i18next from 'i18next';
+import resources from './locales/index';
 // import axios from 'axios';
 // import onChange from 'on-change';
 
-const elements = {
-  form: document.querySelector('form'),
-  input: document.getElementById('url-input'),
-  feedback: document.querySelector('.feedback'),
-  errorField: document.querySelector('.text-danger'),
+const app = async () => {
+  const defaultLanguage = 'ru';
+  const i18nInstance = i18next.createInstance();
+  await i18nInstance.init({
+    lng: defaultLanguage,
+    debug: false,
+    resources,
+  });
+
+  const elements = {
+    form: document.querySelector('form'),
+    input: document.getElementById('url-input'),
+    feedback: document.querySelector('.feedback'),
+    errorField: document.querySelector('.text-danger'),
+  };
+
+  const state = {
+    existedUrls: [],
+    errors: {},
+  };
+  yup.setLocale({
+    mixed: {
+      notOneOf: 'errors.urlExist',
+    },
+    string: {
+      url: 'errors.notUrl',
+    },
+  });
+
+  const validate = (field) => yup
+    .string().trim().required().url()
+    .notOneOf(state.existedUrls)
+    .validate(field);
+
+  elements.form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const inputValue = formData.get('url');
+
+    validate(inputValue).then(() => {
+      elements.input.classList.remove('is-invalid');
+      elements.input.classList.add('is-valid');
+      state.existedUrls.push(inputValue);
+      elements.feedback.classList.remove('text-danger');
+      elements.feedback.classList.add('text-success');
+      elements.feedback.textContent = i18nInstance.t('successLoad');
+      elements.input.value = '';
+      elements.input.focus();
+    })
+      .catch((error) => {
+        elements.input.classList.add('is-invalid');
+        elements.feedback.classList.add('text-danger');
+        elements.feedback.textContent = error.errors.map((errorKey) => i18nInstance.t(errorKey)).join('\n');
+        state.errors = error;
+        console.log(state.errors);
+        console.log(state.existedUrls);
+      });
+  });
 };
 
-const state = {
-  existedUrls: [],
-  errors: {},
-};
+app();
 
-const validate = (field) => yup
-  .string().trim().required().url()
-  .notOneOf(state.existedUrls)
-  .validate(field);
-
-elements.form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const formData = new FormData(e.target);
-  const inputValue = formData.get('url');
-  elements.input.value = '';
-  elements.input.focus();
-
-  validate(inputValue).then(() => {
-    elements.input.classList.remove('is-invalid');
-    elements.input.classList.add('is-valid');
-    state.existedUrls.push(inputValue);
-    // eslint-disable-next-line no-console
-    console.log('done');
-  })
-    .catch((error) => {
-      elements.input.classList.add('is-invalid');
-      state.errors = error;
-      // eslint-disable-next-line no-console
-      console.log(state.errors);
-      // eslint-disable-next-line no-console
-      console.log(state.existedUrls);
-    });
-});
+export default app;
---
+++ src/locales/ru.js
@@ -0,0 +1,10 @@
+export default {
+  translation: {
+    lng: 'Русский',
+    successLoad: 'RSS успешно загружен',
+    errors: {
+      notUrl: 'Ссылка должна быть валидным URL',
+      urlExist: 'RSS уже существует',
+    },
+  },
+};
